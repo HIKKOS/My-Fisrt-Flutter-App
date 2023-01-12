4de1424406f823e447ca61ba29c14d55ca059b58
Hikkos <noeparedes027@gmail.com>
2023-01-12 10:03:20 -0600

    Agregado el boton

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:my_awesome_namer/pages/MyHome.dart';
+import 'package:my_awesome_namer/states/myappstate.dart';
+import 'package:provider/provider.dart';
+
+main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => MyAppState(),
+        child: MaterialApp(
+          title: 'namer App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple)),
+          home: MyHomePage(),
+        ));
+  }
+}
---
+++ lib/pages/MyHome.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:my_awesome_namer/states/myappstate.dart';
+import 'package:provider/provider.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random AWESOME idea:'), // ← Example change.
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
+        ],
+      ),
+    );
+  }
+}
